fe349777fcb37856ad9aaa9cc583dcc128e3e035
daniel-s-souza <danielsantsouza94@gmail.com>
2023-04-13 16:15:01 -0300

    feat: implementa logicas de signin

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useAuth from '../../hooks/useOut';
+import Button from "../../components/button";
+import { Link, useNavigate } from 'react-router-dom';
+import Input from "../../components/inputs";
+import * as C from './style';
 
 const Signin = () => {
+  const { signin } = useAuth();
+  const navigate = useNavigate();
+  
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    if ( !email || !senha) {
+      setError("Preencha todos os campos");
+      return;
+    }
+
+    const res = signin(email, senha)
+
+    if (res) {
+      setError(res);
+      return;
+    }
+
+    navigate("/home");
+  }
+
   return (
-    <div>Signin</div>
+   <C.Container>
+      <C.Label>Login</C.Label>
+      <Input 
+        type="email"
+        placeholder="Digite seu e-mail"
+        value={email}
+        onChange={(e) => [setEmail(e.target.value), setError("")]}
+      />
+        <Input 
+        type="password"
+        placeholder="Digite sua senha"
+        value={senha}
+        onChange={(e) => [setSenha(e.target.value), setError("")]}
+      />
+      <C.LabelError>{error}</C.LabelError>
+      <Button Text="Entrar" onClick={ handleLogin } />
+      <C.LabelSignup>
+        Não tem cadastro?
+        <C.Strong>
+          <Link to= "/signup">&nbsp;Registre-se</Link>
+        </C.Strong>
+      </C.LabelSignup>
+   </C.Container>
   )
 }
 
