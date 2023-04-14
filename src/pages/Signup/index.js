d6cd9fc7de4ef19e8b5c2b9d64fb31b6eeea79d6
daniel-s-souza <danielsantsouza94@gmail.com>
2023-04-14 14:04:16 -0300

    feat: seta os estados da pagina de cadastro

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react'
+import useAuth from '../../hooks/useOut';
+import { Link, useNavigate } from 'react-router-dom';
 import Input from '../../components/inputs';
 import Button from '../../components/button';
 import * as C from './style';
 
 const Signup = () => {
+
+  const [email, setEmail] = useState("")
+  const [emailConf, setEmailConf] = useState("")
+  const [senha, setSenha] = useState("")
+  const [error, setError] = useState("")
+
   return (
     <div>Signup</div>
   )
