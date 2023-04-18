3ce70e1abebe58bb5659b2af184c8cfd79fc1e0f
daniel-s-souza <danielsantsouza94@gmail.com>
2023-04-18 15:04:11 -0300

    feat: implementa rota de logout e implementa link que ao clicar limpa o token de login do local storage

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import * as C from './style';
+import { Link } from 'react-router-dom';
+import useAuth from '../../hooks/useOut';
+
 
 function Header() {
+
+  const { signOut } = useAuth();
+
+  const handleSignout = () => {
+    signOut();
+  }
+  
   return (
     <C.Header>
-      
+      <C.Content>
+        <C.Navigation>
+          <Link to="/home">Home</Link>
+        </C.Navigation>
+        <C.Navigation>
+          <Link to="/home">Perfil</Link>
+        </C.Navigation>
+        <C.Navigation>
+         <Link onClick={handleSignout}>Sair</Link>
+        </C.Navigation>
+      </C.Content>
     </C.Header>
   )
 }
--- src/components/header/style.js
+++ src/components/header/style.js
@@ -2,18 +2,32 @@ import styled from "styled-components";
 
 export const Header = styled.header`
   background-color:  #046ee5;
-  display: flex;
   width: 100vw;
   height: 60px;
   position: fixed;
   box-shadow: 0 1px 2px #0003;
 `
 
+export const Content = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: space-around;   
+  padding: 20px;
+  margin-left:50%;
+`
+
 export const Navigation = styled.strong`
   cursor: pointer;
 
   a {
+    font-size: 18px;
     text-decoration: none;
     color: white;
+  }
+
+  a:hover {
+    opacity: .5;
+    color: black;
   }
 `
