9db0d4fbc16e891b52334ebee2395c647783570e
daniel-s-souza <danielsantsouza94@gmail.com>
2023-04-24 15:29:18 -0300

    feat: implementa lista de bancos

--- src/pages/editProfile/index.js
+++ src/pages/editProfile/index.js
@@ -3,19 +3,32 @@ import { useNavigate } from 'react-router-dom';
 import * as C from './style';
 import Input from '../../components/inputs';
 import Button from '../../components/button'
-import axios from 'axios';
+import { bancos } from './bancos';
+
 
 function EditProfile() {
 
   const [newName, setNewName] = useState("");
   const [newEmail, setNewEmail] = useState("");
   const [banckAccounts, setBanckAccounts] = useState([]);
+  const [banckName, setBanckName] = useState('');
+  const [agNumber, setAgNumber] = useState('');
   const [error, setError] = useState('');
+  const [accType, setAccType] = useState('option1');
 
 const userData = localStorage.getItem('userDb');
 
+
 const handleNewName = () => {
 
+}
+
+const handleSelectChange = (event) => {
+  setBanckName(event.target.value);
+};
+
+const handleOptionChange = (event) => {
+  setAccType(event.target.value);
 }
 
 
@@ -38,7 +51,37 @@ const handleNewName = () => {
             onChange={(e) => [setNewEmail(e.target.value), setError('')]} 
             />
         </C.Text>
-          <Button Text="Salvar" type="submit"/>
+        <>
+        <C.Text>
+          Banco:
+          <C.Select value={banckName} onChange={handleSelectChange}>
+            {bancos.map((banco, index) => (
+              <option key={index} value={banco.label}>{banco.label}</option>
+            ))}
+          </C.Select>
+          <C.Text>
+              Agência:
+              <Input type="text"
+               placeholder="Agência"
+               value={agNumber}
+               onChange={(e) => [setAgNumber(e.target.value), setError('')]}
+               />
+          </C.Text>
+          <C.Text>
+              Conta:
+              <Input type="text"
+               placeholder="Conta"
+               value={banckAccounts}
+               onChange={(e) => [setBanckAccounts(e.target.value), setError('')]}
+               />
+          </C.Text>
+          <C.CheckedDiv>
+            <input type='checkbox' />
+          </C.CheckedDiv>
+        </C.Text>
+        <Button Text="Salvar Conta" type="submit"/>
+        </>
+          <Button Text="Salvar Perfil" type="submit"/>
       </C.Content>
     </C.Container>
   )
--- src/pages/editProfile/style.js
+++ src/pages/editProfile/style.js
@@ -34,3 +34,28 @@ export const Text = styled.p`
   font-size: 20px;
   color: #676767;
 `;
+
+export const Select = styled.select`
+width: 100%;
+height:35px;
+background: white;
+color: gray;
+padding-left: 5px;
+font-size: 14px;
+border:none;
+margin-left: 10px;
+
+     option {
+       color: black;
+       background: white;
+       font-weight: small;
+       display: flex;
+       white-space: pre;
+       min-height: 20px;
+       padding: 0px 2px 1px;
+     }
+`;
+
+export const CheckedDiv = styled.div `
+  display: flex;
+`
