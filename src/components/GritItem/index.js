786de18fbe392fe222d5bee27ffa0ada2330cf21
daniel-s-souza <danielsantsouza94@gmail.com>
2023-04-26 14:52:24 -0300

    feat: implementa corpo do gridItem

--- src/components/GritItem/index.js
+++ src/components/GritItem/index.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import * as C from './style';
+import {
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaTrash,
+} from "react-icons/fa";
 
-function GridItem() {
+function GridItem({ item, onDelete }) {
   return (
-    <div>GridItem</div>
+    <C.Tr>
+      <C.Td>{item.desc}</C.Td>
+      <C.Td>{item.amount}</C.Td>
+      <C.Td alignCenter>
+        {item.expense ? (
+          <FaRegArrowAltCircleDown color='red'/>) : (
+            <FaRegArrowAltCircleUp color='green'/>)
+        }
+      </C.Td>
+      <C.Td alignCenter>
+        <FaTrash onClick={() => onDelete(item.id)} />
+      </C.Td>
+    </C.Tr>
   )
 }
 
