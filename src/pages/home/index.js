8a9c7369502f25e5cc57c47ab71506cc4509bc24
daniel-s-souza <danielsantsouza94@gmail.com>
2023-04-26 11:06:43 -0300

    feat: implementa logica de soma das entradas e saidas

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../../components/header'
 import Resume from '../../components/Resume'
 import Form from '../../components/form'
 
 
 const Home = () => {
+
+  const data = localStorage.getItem("transactions");
+  const [transactionsList, setTransacionsList] = useState(
+    data ? JSON.parse(data) : []
+  );
+    const [income, setIncome] = useState(0);
+    const [expense, setExpense] = useState(0);
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+      const amountExpense = transactionsList
+      .filter((item) => item.expense)
+      .map((transiction) => Number(transiction.amount));
+
+      const amountIncome = transactionsList
+      .filter((item) => !item.expense)
+      .map((transiction) => Number(transiction.amount));
+
+      const expense = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+      const income = amountIncome.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+
+      const total = Math.abs(income - expense).toFixed(2);
+
+      setIncome(`R$ ${income}`);
+      setExpense(`R$ ${expense}`);
+      setTotal(`${Number(income) < Number(expense) ? "-" : ""}R$ ${total}`);
+    }, [transactionsList])
+
   return (
     <>
     <Header />
