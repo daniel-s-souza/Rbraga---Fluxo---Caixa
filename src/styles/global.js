1df607588367fda423820ac38bf5f5c632d25591
daniel-s-souza <danielsantsouza94@gmail.com>
2023-05-19 10:30:15 -0300

    feat: implementa css para scroll bar

--- src/styles/global.js
+++ src/styles/global.js
@@ -12,6 +12,27 @@ body {
   height: 100vh;
   background-color: #f0f2f5;
   font-family: Arial, Helvetica, sans-serif;
+  --sb-track-color: #f0f2f5;
+  --sb-thumb-color: #046ee5;
+  --sb-size: 5px;
+  overflow-x: hidden;
+
+  scrollbar-color: var(--sb-thumb-color) 
+                   var(--sb-track-color)
+}
+
+body::-webkit-scrollbar {
+  width: var(--sb-size);
+}
+
+body::-webkit-scrollbar-track {
+  background: var(--sb-track-color);
+  border-radius: 10px;
+}
+
+body::-webkit-scrollbar-thumb {
+  background: var(--sb-thumb-color);
+  border-radius: 10px;
 }
 `
 export default GlobalStyle;
